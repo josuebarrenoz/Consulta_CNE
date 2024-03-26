cea8c44f949cfdf8b9db15120a6c8393e5e73986
josue <maharba.josue@gmail.com>
2024-03-25 22:24:05 -0400

    Testing Library #2 Name suceed

--- Cne.py
+++ Cne.py
@@ -3,7 +3,7 @@ import PyCNE
 def run():
     Cedula = int(input("Escribe un numero de Cedula: "))
     consulta = PyCNE.consulta('V',Cedula)
-    print(consulta)
+    print(consulta.nombre)
 
 if __name__=="__main__":
     run()
